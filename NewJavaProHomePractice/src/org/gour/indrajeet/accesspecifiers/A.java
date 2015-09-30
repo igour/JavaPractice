c521f72645cdf1072b9a399b291acd4a9ffa0124
indra <ijgour@gmail.com>
2015-09-30 12:42:03 +0530

    global and local variable code

--- NewJavaProHomePractice/src/org/gour/indrajeet/accesspecifiers/A.java
+++ NewJavaProHomePractice/src/org/gour/indrajeet/accesspecifiers/A.java
@@ -1,14 +1,20 @@
 package org.gour.indrajeet.accesspecifiers;
 
 public class A {
-// to investigate for default access specifier
-	public int a = 10;
-	void m1(){
-		System.out.println("Method A.m1 with var "+a);
+	// to investigate for default access specifier
+	public int a;// = 10; // default initialized by zero
+				// not necessary for initialize for use.
+
+	void m1() {
+		int ab = 0; // if you want to initialized than only you can use
+					// else don't initialized and donot use that would also work.
+					// means local variable will not need initialization to use.
+		System.out.println("Method A.m1 with var " + ab);
+
 	}
 	public static void main(String[] args) {
 		A a = new A();
 		a.m1();
-		System.out.println(a.a);;
+		System.out.println(a.a);
 	}
 }
